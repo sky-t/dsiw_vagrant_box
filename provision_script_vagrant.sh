e66b6950b0d495f114f4f536d5116e936e7b0e95
Scott Tse <Scott Tse>
2017-01-27 21:34:33 -0800

    Add Vagrant provision scripts

---
+++ provision_script_vagrant.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo 'Installing miniconda'
+#check to see if the file's already been downloaded (this step could be skiped)
+if [ ! -f ~/proj/Miniconda3-latest-Linux-x86_64.sh ]
+then
+  #if the file hasn't been downlaoded, go get it.
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/proj/Miniconda3-latest-Linux-x86_64.sh
+fi
+
+#execute the install program in batch mode (it won't ask for prompts and won't modify the path)
+bash ~/proj/Miniconda3-latest-Linux-x86_64.sh -b
+
+#modify the path in profile
+echo 'export PATH=~/miniconda3/bin:${PATH}' >> ~/.profile
+
+#modify the path for the rest of this scripts
+export PATH=~/miniconda3/bin:${PATH}
+
+
+echo 'setting up Jupyter'
+conda install -y jupyter 
+
+echo 'setting up cvdjango'
+conda create -y -n cvdjango python django psycopg2 
+source activate cvdjango
+
+pip install djangorestframework 
+
+source deactivate cvdjango
+
+echo 'setting up dotenv'
+pip install -U python-dotenv 
+
+echo 'setting up data analysis tools'
+conda install -y psycopg2 
+conda install -y numpy 
+conda install -y matplotlib 
+conda install -y pandas 
+conda install -y seaborn 
+conda install -y scipy 
+conda install -y statsmodels 
+conda install -y scikit-learn 
+
